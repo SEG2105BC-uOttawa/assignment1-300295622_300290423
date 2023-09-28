7c677092b55744528ea86e5d0bf9d39257c2f998
B-Gilb022 <benjamin.gilbert2004@icloud.com>
2023-09-28 15:56:17 -0400

    Added tests to try and fix ArrayListCreate method (still needs work)

--- code_part2/ArrayTest.java
+++ code_part2/ArrayTest.java
@@ -5,23 +5,28 @@ import java.util.*;
 public class ArrayTest {
     
 
-    public int ArrayListCreate(){
+    public static int ArrayListCreate(){
         ArrayList<String> arr = new ArrayList<String>();
         long startTime = System.nanoTime();
         long afterTime = 0;
         long time = afterTime - startTime;
-        long timeSec = time/(10*9);
+        long timeSec = 0;
+        System.out.println(timeSec);
         while (timeSec < 10){
             arr.add("1");
             afterTime = System.nanoTime();
             time = afterTime - startTime;
-            timeSec = time/(10*9);
+            System.out.println(afterTime);
+            System.out.println(startTime);
+            System.out.println(time);
+            timeSec = time/(10^9);
+            System.out.println(timeSec);
         }
         int x = arr.size();
         return x;
     }
 
-    public void main(String args[]){
+    public static void main(String[] args){
         int a = ArrayListCreate();
         System.out.println(a);
     }
