aa1513a82353b7731751df7da18b1fab689f85c1
B-Gilb022 <benjamin.gilbert2004@icloud.com>
2023-09-28 14:29:46 -0400

    Ben Worked on Part 1
    Designs 2 and 3 complete and tested. Design 5 has issue, needs to be fixed

---
+++ pointcp/design5/PointCP3.java
@@ -0,0 +1,86 @@
+package design5;
+
+// This file contains material supporting section 2.9 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at http://www.site.uottawa.ca/school/research/lloseng/
+
+/**
+ * This class contains instances of coordinates in either polar or
+ * cartesian format.  It also provides the utilities to convert
+ * them into the other type. It is not an optimal design, it is used
+ * only to illustrate some design issues.
+ *
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Dr Timothy C. Lethbridge
+ * @version July 2000
+ */
+public class PointCP3 extends PointCP5
+{
+  //Instance variables ************************************************
+  
+  /**
+   * Contains the current value of X or RHO depending on the type
+   * of coordinates.
+   */
+  private double xOrRho;
+  
+  /**
+   * Contains the current value of Y or THETA value depending on the
+   * type of coordinates.
+   */
+  private double yOrTheta;
+	
+  
+  //Constructors ******************************************************
+
+  /**
+   * Constructs a coordinate object, with a type identifier.
+   */
+  public PointCP3(char type, double xOrRho, double yOrTheta)
+  {
+    super('C', xOrRho, yOrTheta);
+  }
+	
+  
+  //Instance methods **************************************************
+ 
+ 
+  public double getX()
+  {
+    return xOrRho;
+  }
+  
+  public double getY()
+  {
+    return yOrTheta;
+  }
+  
+  public double getRho()
+  {
+    return (Math.sqrt(Math.pow(xOrRho, 2) + Math.pow(yOrTheta, 2)));
+  }
+  
+  public double getTheta()
+  {
+    return Math.toDegrees(Math.atan2(yOrTheta, xOrRho));
+  }
+
+  /**
+   * Rotates the specified point by the specified number of degrees.
+   * Not required until E2.30
+   *
+   * @param point The point to rotate
+   * @param rotation The number of degrees to rotate the point.
+   * @return The rotated image of the original point.
+   */
+  public PointCP3 rotatePoint(double rotation)
+  {
+    double radRotation = Math.toRadians(rotation);
+    double X = getX();
+    double Y = getY();
+        
+    return new PointCP3('C',
+      (Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y),
+      (Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y));
+  }
+}
--- pointcp/design5/PointCPTest5.java
+++ pointcp/design5/PointCPTest5.java
@@ -1,3 +1,5 @@
+package design5;
+
 // This file contains material supporting section 2.9 of the textbook:
 // "Object Oriented Software Engineering" and is issued under the open-source
 // license found at http://www.site.uottawa.ca/school/research/lloseng/
@@ -13,7 +15,7 @@ import java.io.*;
  * @author Paul Holden
  * @version July 2000
  */
-public class PointCPTest
+public class PointCPTest5
 {
   //Class methods *****************************************************
 
@@ -33,7 +35,7 @@ public class PointCPTest
    */
   public static void main(String[] args)
   {
-    PointCP point;
+    PointCP2 point;
 
     System.out.println("Cartesian-Polar Coordinates Conversion Program");
 
@@ -42,7 +44,7 @@ public class PointCPTest
     // If he did not, prompt the user for them.
     try
     {
-      point = new PointCP(args[0].toUpperCase().charAt(0), 
+      point = new PointCP2(args[0].toUpperCase().charAt(0), 
         Double.valueOf(args[1]).doubleValue(), 
         Double.valueOf(args[2]).doubleValue());
     }
@@ -80,7 +82,7 @@ public class PointCPTest
    * @throws IOException If there is an error getting input from
    *         the user.
    */
-  private static PointCP getInput() throws IOException
+  private static PointCP2 getInput() throws IOException
   {
     byte[] buffer = new byte[1024];  //Buffer to hold byte input
     boolean isOK = false;  // Flag set if input correct
@@ -158,7 +160,7 @@ public class PointCPTest
       isOK = false;
     }
     //Return a new PointCP object
-    return (new PointCP(coordType, a, b));
+    return (new PointCP2(coordType, a, b));
   }
 }
 
